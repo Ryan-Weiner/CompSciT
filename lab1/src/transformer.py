6bb6f72562ea8b99eaac2f81984f0a3e802003e6
ryanWeiner6425 <112270546+ryanWeiner6425@users.noreply.github.com>
2022-08-27 16:57:46 -0400

    Update transformer.py
    final working product to be submitted for grading

--- lab1/src/transformer.py
+++ lab1/src/transformer.py
@@ -1,6 +1,5 @@
 """
 CSAPX Lab 1: Secret Messages
-
 A program that encodes/decodes a message by applying a set of transformation operations.
 The transformation operations are:
     shift - Sa[,n] changes letter at index a by moving it n letters fwd in the alphabet. A negative
@@ -8,44 +7,92 @@ The transformation operations are:
     rotate - R[n] rotates the string n positions to the right. A negative value for n rotates the string
         to the left.
     duplicate - Da[,n] follows character at index a with n copies of itself.
-
 All indices numbers (the subscript parameters) are 0-based.
 
 author: Ryan Weiner
 """
-def transform(opString, message):
-    ops = opString.split(';')
-    operation = ""
-    for op in ops:
-        operation = op[0]
-        param = op[1:]
-        params = param.split(',')
-        words = {"R":"Rotate", "S":"Shift", "D":"Duplicate", "T":"Swap"}
-        paramMessage = "Operation {} {} - Parameters: ".format(words[operation], operation)
-        if len(params)> 1:
-            paramMessage += "{}, {}".format(params[0], params[1])
+def shift(message : str, index : int) -> str:
+    '''shifts the message[index] forward in the alphabet num number of positions.  wraps around if past z'''
+    newChar = chr(ord('A')+(ord(message[index])-ord('A')+1)%26)
+    return message[:index]+newChar+message[index+1:]
+def rotate(message : str, num : int) -> str:
+    '''rotates characters in message forward num number of positions'''
+    return message[-num:]+message[:len(message)-num]
+def duplicate(message : str, index : int) -> str:
+    '''duplicates character in message at index'''
+    return message[:index]+message[index]+message[index:]
+def swap(message : str, index1 : int, index2 : int) -> str:
+    '''swaps characters in message at index1 and index2'''
+    message = list(message)
+    message[index1], message[index2] = message[index2], message[index1]
+    return ''.join(message)
+def unDuplicate(message : str, index : int) -> str:
+    '''deletes the duplicated character in message at index'''
+    return message[:index]+message[index+1:]
+def unRotate(message : str, num : int) -> str:
+    '''rotates message backward num positions'''
+    return message[num:]+message[:num]
+def getOpType(op : str) -> str:
+    '''returns a single character value indicating the type of transformation'''
+    return op[0]
+def createOpList(opString : str) -> str:
+    '''splits the parameters of the operation into a list to be used later'''
+    return opString.split(',')
+def callTransf(tType : str, message : str, encrypt : bool, params) -> str:
+    '''calls the respective transformation function based on parameters.  returns the message post-transformation'''
+    if tType == "T":
+        message = swap(message, int(params[0]), int(params[1]))
+    if encrypt:
+        if tType == "R":
+            message = rotate(message, params)
+        if tType == "S":
+            message = shift(message, int(params[0]))
+        if tType == "D":
+
+            message = duplicate(message, int(params[0]))
+    if not encrypt:
+        if tType == "R":
+            message = unRotate(message, params)
+        if tType == "S":
+            message = shift(message, -int(params[0]))
+        if tType == "D":
+            message = unDuplicate(message, int(params[0]))
+    return message
+def printParamsMessage(op : str):
+    '''prints the message describing which transformations are being used with which parameters.  returns the parameter list for later use'''
+    operation = op[0]
+    param = op[1:]
+    params = param.split(',')
+    words = {"R": "Rotate", "S": "Shift", "D": "Duplicate", "T": "Swap"}
+    paramMessage = "Operation {} {} - Parameters: ".format(words[operation], operation)
+    if len(params) > 1:
+        paramMessage += "{}, {}".format(params[0], params[1])
+    else:
+        paramMessage += params[0]
+    print(paramMessage)
+    return params
+def encrypt(opString : str, message : str) -> str:
+    '''called if the user would like to encrypt a message.  takes operations to use and applies them to the message. returns encrypted message'''
+    for op in opString.split(';'):
+        message = execute(printParamsMessage(op), op[0], message, True)
+    return message
+def decrypt(opString: str, message : str) -> str:
+    '''called to decrypt message with the operations that were used to encrypt it.  returns decrypted message'''
+    for op in opString.split(';')[::-1]:
+        message = execute(printParamsMessage(op), op[0], message, False)
+    return message
+def execute(params, operation , message, encrypt) -> str:
+    '''executes the correct transformation functions, returns post transformation message'''
+    for i in range(len(params)):
+        if params[i] != '':
+            params[i] = int(params[i])
         else:
-            paramMessage+=params[0]
-        print(paramMessage)
-    print("\n\n")
-    opList = []
-    opList.append(op for op[0] in ops)
-    print(o for o in opList)
-
-
-def shift(message):
-    pass
-def rotate(message):
-    pass
-def duplicate(message, index):
-    pass
-def swap(message, index1, index2):
-    pass
-def encrypt(opString, message):
-    pass
-def decrypt(opString, message):
-    pass
-
+            params = 1
+            break
+    message = callTransf(operation, message, encrypt, params)
+    print(message)
+    return message
+print(rotate("HOESS", 1))
 def main() -> None:
     """
     The main loop responsible for getting the input details from the user
@@ -53,7 +100,6 @@ def main() -> None:
     of encrypting or decrypting the message applying the transformations
     :return: None
     """
-
     while True:
         uChoice = (input("Would you like to encrypt (E), decrypt(D) or quit (Q)? ")).upper()
         if uChoice == "Q":
@@ -64,14 +110,10 @@ def main() -> None:
         else:
             message = (input("Enter the message: ")).upper()
             encryptOps = input("Enter the encrypting transformation operations: ")
-            print("Generating output...")
-            print("Transforming message: {}".format(message))
-            print("Applying...")
+            print("Generating output...\nTransforming message: {}\nApplying...".format(message))
             if uChoice == "D":
                 decrypt(encryptOps, message)
             elif uChoice == "E":
                 encrypt(encryptOps, message)
-            transform(encryptOps, message)
-
 if __name__ == '__main__':
     main()
